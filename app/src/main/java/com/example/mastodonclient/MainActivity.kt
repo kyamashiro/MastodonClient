218fdd47233bbfb14f4fef855ed10d0fef0ec086
Kazuki Yamashiro <kyamashiro73@gmail.com>
2020-04-30 08:04:44 +0900

    リソースの取得の仕組み、表示テキストの変更

--- app/src/main/java/com/example/mastodonclient/MainActivity.kt
+++ app/src/main/java/com/example/mastodonclient/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mastodonclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // リソースのIDがtextviewのオブジェクトを取得する
+        val textView: TextView = findViewById(R.id.textview)
+        // 取得したオブジェクトのtext変数に代入する
+        textView.text = "Hello XML Layout!"
     }
 }
