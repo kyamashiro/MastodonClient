80f13ba84f8fcf34345569c9ee66c167ea226938
Kazuki Yamashiro <kyamashiro73@gmail.com>
2020-05-04 08:36:13 +0900

    ImageViewでAPIから取得したプレビュー画像を表示する

--- app/src/main/java/com/example/mastodonclient/DataBindingAdapter.kt
+++ app/src/main/java/com/example/mastodonclient/DataBindingAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.mastodonclient
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
 // 既存のクラスを拡張しているっぽい
 // https://jumperson.hatenablog.com/entry/2016/06/07/135256
 // https://qiita.com/pinemz/items/640428706dac1bfae409#kotlin-%E3%81%A7%E3%81%AE%E6%8B%A1%E5%BC%B5%E9%96%A2%E6%95%B0%E3%81%A8%E3%81%9D%E3%81%AE%E5%AE%9F%E8%A3%85
@@ -12,4 +15,15 @@ fun TextView.setSpannedString(content: String) {
         content,
         HtmlCompat.FROM_HTML_MODE_COMPACT
     )
+}
+
+@BindingAdapter("media")
+fun ImageView.setMedia(media: Media?) {
+    if (media == null) {
+        setImageDrawable(null)
+        return
+    }
+    Glide.with(this)
+        .load(media.url)
+        .into(this)
 }
--- app/src/main/java/com/example/mastodonclient/Toot.kt
+++ app/src/main/java/com/example/mastodonclient/Toot.kt
@@ -4,10 +4,14 @@ import com.squareup.moshi.Json
 
 data class Toot(
     val id: String,
-    // JSONのキーとフィールド名を変わる場合などでは、@Json アノテーションを付ける
-    @Json(name = "created_at") val createdAt: String,
     val sensitive: Boolean,
     val url: String,
     val content: String,
-    val account: Account
-)
+    @Json(name = "media_attachments") val mediaAttachments: List<Media>,
+    val account: Account,
+    // JSONのキーとフィールド名を変わる場合などでは、@Json アノテーションを付ける
+    @Json(name = "created_at") val createdAt: String
+) {
+    val topMedia: Media?
+        get() = mediaAttachments.firstOrNull()
+}
