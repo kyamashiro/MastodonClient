229866e536160aab4cf586dee0a0c9879aa83d8a
Kazuki Yamashiro <kyamashiro73@gmail.com>
2020-05-06 14:55:15 +0900

    ボトムナビゲーションを追加

--- app/src/main/java/com/example/mastodonclient/ui/MainActivity.kt
+++ app/src/main/java/com/example/mastodonclient/ui/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.mastodonclient.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mastodonclient.R
+import com.example.mastodonclient.databinding.ActivityMainBinding
 import com.example.mastodonclient.ui.toot_list.TootListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // bottomナビゲーションUI
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.menu_home -> TootListFragment()
+                R.id.menu_public -> TootListFragment()
+                else -> null
+            }
+            fragment ?: return@setOnNavigationItemSelectedListener false
+
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    fragment,
+                    TootListFragment.TAG
+                )
+                .commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
+
         // 画面が再生成されたときのことを考慮して画面がはじめて作成された時ににだけFragmentを追加するようにする
         if (savedInstanceState == null) {
             val fragment = TootListFragment()
